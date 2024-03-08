9472383ea15edc38e1f7e5959ce619c55f359fbd
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-03-08 12:33:05 +0100

    Add route parameter logging in users page

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,9 @@
 <script setup>
 const route = useRoute();
-console.log(route.params);
 </script>
 
 <template>
   <div>
-    <p>Category: # {{ $route.params.id }}</p>
+    <p>Home</p>
   </div>
 </template>
