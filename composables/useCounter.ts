3f7a39bf9a36fd8bd8f425d84f7a46808056b10c
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-03-08 14:55:02 +0100

    Delete counter.ts and update index.vue to use a counter

--- composables/useCounter.ts
+++ composables/useCounter.ts
@@ -2,7 +2,11 @@ export const useCounter = () => {
   const count = ref(0);
 
   const increment = () => count.value++;
-  const decrement = () => count.value--;
+  const decrement = () => {
+    if (count.value > 0) {
+      count.value--;
+    }
+  };
 
   return {
     count,
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,19 @@
-<script setup></script>
-
+<script setup>
+const counter = useCounter();
+</script>
 <template>
-  <div>
-    <p>Home</p>
+  <div class="w-full h-[100vh]">
+    <h1>Home</h1>
+    <div class="flex-col">
+      Counter: {{ counter.count }}
+      <button @click="counter.increment">+</button>
+      <button @click="counter.decrement">-</button>
+    </div>
   </div>
 </template>
+
+<style scoped>
+button {
+  @apply border-2 border-black px-4 mx-2 text-lg bg-lime-300;
+}
+</style>
