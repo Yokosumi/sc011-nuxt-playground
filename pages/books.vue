25f22e40c974878e3e1ce88f86cb548ed30421f2
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-03-08 14:26:42 +0100

    Refactor books.vue to improve UI and display book details

--- pages/books.vue
+++ pages/books.vue
@@ -12,10 +12,20 @@ const { data: books } = useFetch<Book[]>(url);
 
 <template>
   <div>
-    <h1>There are {{ books?.length }} books available</h1>
+    <h1 class="text-xl">
+      There are
+      <span class="font-bold text-green-400">{{ books?.length }}</span> books
+      available
+    </h1>
     <ul>
-      <li v-for="book in books" :key="book.title">
-        {{ book.title }} ({{ book.language }}) - {{ book.rank }}
+      <li
+        v-for="book in books"
+        :key="book.title"
+        class="my-2 border-2 max-w-fit-content p-2 rounded-md bg-gradient-to-l from-gray-500 to-blue-200"
+      >
+        <p>Title: {{ book.title }}</p>
+        <p>Language: {{ book.language }}</p>
+        <p>Rank: {{ book.rank }}</p>
       </li>
     </ul>
   </div>
