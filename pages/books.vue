e703aa68a34c2ed9e8cee9a25a4ddd248549c916
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-03-10 16:44:20 +0100

    Update useFetch type in books.vue

--- pages/books.vue
+++ pages/books.vue
@@ -7,7 +7,7 @@ interface Book {
 
 const url = 'https://edwardtanguay.vercel.app/share/books.json';
 
-const { data: books } = await useFetch<Book[]>(url);
+const { data: books } = await useFetch<Book[] | null>(url);
 </script>
 
 <template>
