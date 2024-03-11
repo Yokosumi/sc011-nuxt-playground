db5cfc5930e1468b187f0597436f1c862eb9cda9
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-03-11 01:01:45 +0100

    Refactor products page to use ProductCard component

---
+++ components/ProductCard.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { Product } from '../types';
+
+type props = {
+  product: Product;
+};
+
+const { product } = defineProps<props>();
+</script>
+
+<template>
+  <section
+    :v-for="product"
+    class="flex flex-col shadow-md bg-white p-6 rounded-md"
+  >
+    <img class="w-[75px] h-auto self-center" :src="product.image" alt="" />
+    <h2 class="text-black mt-auto text-sm">
+      {{ product.title }}
+    </h2>
+  </section>
+</template>
--- pages/products.vue
+++ pages/products.vue
@@ -1,24 +1,24 @@
-<template>
-  <div v-if="pending">
-    <p>Loading...</p>
-  </div>
-  <div v-else class="grid grid-cols-5 gap-4">
-    <section
-      v-for="product in products"
-      class="flex flex-col shadow-md bg-white p-6 rounded-md"
-    >
-      <img class="w-[75px] h-auto self-center" :src="product.image" alt="" />
-      <h2 class="text-black mt-auto text-sm">
-        {{ product.title }}
-      </h2>
-    </section>
-  </div>
-</template>
+<script setup lang="ts">
+import type { Product } from '../types';
 
-<script setup>
 const url = 'https://fakestoreapi.com/products';
 
-const { pending, data: products } = useFetch(url, {
+const { pending, data: products } = useFetch<Product[]>(url, {
   lazy: false,
 });
 </script>
+
+<template>
+  <section v-if="pending">
+    <p>Loading...</p>
+  </section>
+  <section v-else class="grid grid-cols-5 gap-4">
+    <ProductCard
+      v-for="product in products"
+      :key="product.id"
+      :product="product"
+      :title="product.title"
+      :image="product.image"
+    />
+  </section>
+</template>
